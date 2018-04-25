6e989d4f1694f7a30466238a4d5c2a6aa3078d0a
yunosuke <yunosuke.tanaka.i@finc.com>
2018-04-25 15:07:08 +0900

    admin_specをが綺麗にかけた！

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -9,10 +9,44 @@ RSpec.describe Admin, type: :model do
   end
 
   # error without name
-  # error without email
-  # error without password
-  # error if the email is already in the db
-  # error if the password 2 is different from pass 1
+  it 'is invalid without a name' do
+    admin = build(:admin, name: nil)
+    admin.valid?
+    expect(admin.errors[:name].size).to eq(1)
+  end
 
+  # error without email
+  it 'is invalid without an email' do
+    admin = build(:admin, email: nil)
+    admin.valid?
+    expect(admin.errors[:email].size).to eq(1)
+  end
+
+  # error without password
+  it 'is invalid without an password' do
+    admin = build(:admin, password: nil)
+    admin.valid?
+    expect(admin.errors[:password].size).to eq(1)
+  end
+
+  # error if the email is already in the db
+  it 'is invalid if emils is duplicate' do
+    create(:admin, email: "hoge@example.com")
+    admin = build(:admin, email: "hoge@example.com")
+    admin.valid?
+    expect(admin.errors[:email].size).to eq(1)
+  end
+
+  # error if the password is fewer than 6 letters
+  it 'is invalid if password is smaller than 6 letters' do
+    admin = build(:admin, password: 'hogge')
+    expect(admin.errors[:password].size).to eq(1)
+  end
+
+  # error if the password is longer than 14 letters
+  it 'is invalid if password is longer than 14 letters' do
+    admin = build(:admin, password: 'mogemogemogemog')
+    expect(admin.errors[:password].size).to eq(1)
+  end
 
 end
